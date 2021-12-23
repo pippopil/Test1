75235d4b887f5ca1eca56785f9d908858aba5920
pippopil <voood234@gmail.com>
2021-12-23 15:09:50 +0300

    Add

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 24_500;
+        int fixedAmount = 20;
+        int bonus = ticketPrice/fixedAmount;
+
+        System.out.println(bonus);
+    }
+}
